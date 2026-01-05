94015df7b0ca21811a352fa66f0b37a48802e143
Sohammj <soham0144@gmail.com>
2026-01-05 15:25:51 +0530

    yay

--- components/sections/HandwrittenNotesSection.tsx
+++ components/sections/HandwrittenNotesSection.tsx
@@ -3,30 +3,42 @@ import React from "react";
 
 export default function HandwrittenNotesSection() {
   return (
-    // <section className="relative py-28 md:py-44 overflow-hidden bg-[url('/paper-texture.jpg')] bg-repeat">
     <section className="relative py-36 md:py-56 overflow-hidden bg-[url('/paper-texture.jpg')] bg-repeat">
 
+      {/* ===== YELLOW BLOB ===== */}
+      {/* <div className="absolute right-[8%] top-[8%] z-0 pointer-events-none"> */}
+      <div className="absolute -right-[22%] top-[8%] z-0 pointer-events-none">
 
-      {/* ===== MOBILE (STACKED – OK AS IS) ===== */}
-      <div className="flex flex-col items-center gap-10 md:hidden">
+        <Image
+          src="/yellow-blob.svg"
+          alt=""
+          width={520}
+          height={360}
+          className="opacity-80 rotate-[-6deg] blur-[1px]"
+        />
+      </div>
+
+      {/* ===== MOBILE ===== */}
+      <div className="relative z-10 flex flex-col items-center gap-10 md:hidden">
         <CardImage src="/handwritten-cards-left.png" width={520} height={360} />
-        <CardImage src="/handwritten-cards-middle.png" width={620} height={440} priority />
+        <CardImage
+          src="/handwritten-cards-middle.png"
+          width={620}
+          height={440}
+          priority
+        />
         <CardImage src="/handwritten-cards-right.png" width={520} height={360} />
       </div>
 
-      {/* ===== DESKTOP (MATCHES OLD WORKING LAYOUT) ===== */}
-      <div className="relative max-w-6xl mx-auto h-[420px] isolate hidden md:block">
+      {/* ===== DESKTOP ===== */}
+      <div className="relative z-10 max-w-6xl mx-auto h-[420px] isolate hidden md:block">
 
         {/* LEFT CARD */}
-        <div className="absolute left-0 top-[70px] -translate-x-[45px] scale-[1.0] z-10">
-          <CardImage
-            src="/handwritten-cards-left.png"
-            width={520}
-            height={360}
-          />
+        <div className="absolute left-0 top-[70px] -translate-x-[45px] z-10">
+          <CardImage src="/handwritten-cards-left.png" width={520} height={360} />
         </div>
 
-        {/* CENTER CARD (HERO) */}
+        {/* CENTER CARD */}
         <div className="absolute left-1/2 top-[10px] -translate-x-1/2 scale-[1.12] z-20">
           <CardImage
             src="/handwritten-cards-middle.png"
@@ -37,17 +49,13 @@ export default function HandwrittenNotesSection() {
         </div>
 
         {/* RIGHT CARD */}
-        <div className="absolute right-0 top-[70px] translate-x-[50px] scale-[1.0] z-10">
-          <CardImage
-            src="/handwritten-cards-right.png"
-            width={520}
-            height={360}
-          />
+        <div className="absolute right-0 top-[70px] translate-x-[50px] z-10">
+          <CardImage src="/handwritten-cards-right.png" width={520} height={360} />
         </div>
       </div>
 
       {/* ===== WATER WAVES ===== */}
-      <div className="absolute bottom-0 left-0 w-full pointer-events-none">
+      <div className="absolute bottom-0 left-0 w-full pointer-events-none z-0">
         <Image
           src="/watercolor-waves.png"
           alt=""
